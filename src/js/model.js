f6d5211916b9b983f4ee6babb54099ffa059e748
RSP-007 <217321612+RSP-007@users.noreply.github.com>
2026-07-01 22:54:39 +0530

    Added helpers and configuration files

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,14 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helper.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
-    console.log(res, data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     let { recipe } = data.data;
     state.recipe = {
@@ -25,6 +23,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 💥💥`);
   }
 };
