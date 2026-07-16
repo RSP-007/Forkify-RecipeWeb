4201dac3e25fa35e5cdbdccf4129b585482cd97d
RSP-007 <217321612+RSP-007@users.noreply.github.com>
2026-07-16 22:48:56 +0530

    Add recipe upload 1.0

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import resultsView from './views/resultsView.js';
 
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -83,6 +84,10 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.generateRender(controlRecipe);
@@ -90,6 +95,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
 // window.addEventListener('hashchange', showRecipe);
